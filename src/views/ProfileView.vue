0c9067015d9f6e0795d6eb3ee2d13d6c2bc94947
emilicos <notalvaroaustin@gmail.com>
2023-05-24 21:14:33 +0700

    feat: implement fetching profile data when rendering

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -7,6 +7,7 @@ import { EnvelopeIcon, IdentificationIcon, UserIcon } from '@heroicons/vue/24/so
 import InputField from '@/components/common/InputField.vue';
 import {useToast} from 'vue-toast-notification';
 import 'vue-toast-notification/dist/theme-bootstrap.css';
+import Cookies from 'js-cookie';
 
 export default defineComponent({
   name: 'Login',
@@ -40,27 +41,46 @@ export default defineComponent({
     const email = ref("")
     const username = ref("")
     const toast = useToast();
-
+    const showComponent = ref(false)
+    const role = ref("")
+    const token = Cookies.get('token');
+    
     const showSuccessToast = (message: string) => {
       toast.success(message);
     };
 
     onMounted(() => {
-      
+      axios.get("http://34.124.246.185/api/auth/verify", {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+      })
+      .then((res) => {
+        name.value = res.data.user.name;
+        email.value = res.data.user.email;
+        username.value = res.data.user.username;
+        role.value = res.data.user.role;
+        showComponent.value = true
+      })
+      .catch((err) => {
+        console.log(err)
+      })
     });
 
     return {
         name,
+        role,
         email,
         username,
         showSuccessToast,
+        showComponent
     }
-  }
+  },
 })
 </script>
 
 <template>
-    <div class = "flex flex-col m-0 p-0 w-full gap-y-12">
+    <div v-show=showComponent class = "flex flex-col m-0 p-0 w-full gap-y-12">
         <h1 class = "capitalize text-5xl font-bold text-white w-full"> {{ $route.name }}</h1>
 
         <div class = "flex lg:flex-row flex-col border-2 border-black profile rounded-3xl gap-8 py-12 px-6 mx-0">
@@ -72,11 +92,19 @@ export default defineComponent({
                 <div class = "mx-0 flex flex-col gap-y-6 lg:text-base md:text-sm text-xs">
                     <div class = "flex mx-0 gap-x-[52px] sm:flex-row flex-col max-sm:items-center">
                         <h2 class = "capitalize"> Email </h2>
-                        <p class = "font-bold text-violet"> example@gmail.com </p>
+                        <p class = "font-bold text-violet"> {{ email }}</p>
                     </div>
                     <div class = "flex mx-0 gap-x-12 sm:flex-row flex-col max-sm:items-center">
                         <h2 class = "capitalize"> Name </h2>
-                        <p class = "font-bold text-violet"> John Doe</p>
+                        <p class = "font-bold text-violet"> {{ name }}</p>
+                    </div>
+                    <div class = "flex mx-0 gap-x-12 sm:flex-row flex-col max-sm:items-center">
+                        <h2 class = "capitalize"> Username </h2>
+                        <p class = "font-bold text-violet"> {{ username }}</p>
+                    </div>
+                    <div class = "flex mx-0 gap-x-12 sm:flex-row flex-col max-sm:items-center">
+                        <h2 class = "capitalize"> Role </h2>
+                        <p class = "font-bold text-violet capitalize"> {{ role }}</p>
                     </div>
                 </div>
                 <button className = "max-w-full px-8 py-2 text-center rounded-xl font-bold bg-indigo text-white" @click="openModal"> Edit Profile </button>
